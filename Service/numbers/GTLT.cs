0799b06b323c41b2f1051525dbd031bc6f07414f
Aiden Foo <aidenfoo154@gmail.com>
2024-01-09 14:59:27 -0800

    getting rid of white spcae

--- Service/numbers/GTLT.cs
+++ Service/numbers/GTLT.cs
@@ -6,11 +6,11 @@ public class GTLT : iGTLT
     {
         if (num1 > num2)
         {
-            return $"{num1} is greater than {num2}\n {num2} is less than {num1}";
+            return $"{num1} is greater than {num2}\n{num2} is less than {num1}";
         }
         else if (num1 < num2)
         {
-            return $"{num2} is greater than {num1} \n {num1} is less than {num2}";
+            return $"{num2} is greater than {num1} \n{num1} is less than {num2}";
         }
         else
         {
